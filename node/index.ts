b6a80bd8a1269ea4dd77af342d0ca63c5572406d
Estacio Pereira <estacio.pereira@vtex.com.br>
2021-03-04 14:32:24 -0300

    Bump node builder from 4.x to 6.x

--- node/index.ts
+++ node/index.ts
@@ -1,10 +1,11 @@
+import type { RecorderState, ParamsContext } from '@vtex/api'
 import { Service } from '@vtex/api'
 
 import { resolvers } from './resolvers'
 import type { Clients } from './clients'
 import { clients } from './clients'
 
-export default new Service<Clients>({
+export default new Service<Clients, RecorderState, ParamsContext>({
   clients,
   graphql: {
     resolvers,
