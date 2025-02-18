bfe976c29f41cc3ee45c02cdbb6d1fb02a3b6058
Iago Espinoza <iago1501@gmail.com>
2025-02-18 16:37:59 -0300

    remove unused client

--- node/index.ts
+++ node/index.ts
@@ -1,19 +1,7 @@
 import { resolvers } from './resolvers'
-import { Clients } from './clients'
-import { ClientsConfig, Service } from '@vtex/api'
-
-const clients: ClientsConfig<Clients> = {
-  implementation: Clients,
-  options: {
-    default: {
-      retries: 2,
-      timeout: 2000,
-    },
-  }
-}
+import { Service } from '@vtex/api'
 
 export default new Service({
-  clients,
   graphql: {
     resolvers
   }
