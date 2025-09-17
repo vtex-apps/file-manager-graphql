5a7d57a8fe572c9e15c9cb967873240e6a7a621c
Iago Espinoza <iago1501@gmail.com>
2025-09-17 09:54:44 -0300

    Update auth.ts

--- node/directives/auth.ts
+++ node/directives/auth.ts
@@ -11,8 +11,8 @@ export const authFromCookie = async (ctx: any, operationName: string) => {
 
   const vtexIdToken =
     ctx.cookies.get('VtexIdclientAutCookie') ??
-    ctx.cookies.get('VtexIdclientAutCookie_' + ctx.vtex.account) ??
-    ctx.request.header.vtexidclientautcookie  
+    ctx.request.header.vtexidclientautcookie ??
+    ctx.cookies.get('VtexIdclientAutCookie_' + ctx.vtex.account)
 
   if (!vtexIdToken) {
     return 'User must be logged to access this resource'
